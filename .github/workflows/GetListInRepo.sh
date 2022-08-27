2f44fcb03cf3d5caa51e72da0d933ba04dfb3f95
Tsai Syun <65594575+dino920135@users.noreply.github.com>
2022-08-28 05:09:21 +0800

    Create GetListInRepo.sh

---
+++ .github/workflows/GetListInRepo.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git clone https://github.com/dino920135/Notes
+cd Notes
+ls
+
+data_path='pages'
+file_pattern='*.md'
+
+for directory in $(find ${data_path} -name ${file_pattern})
+do
+    # echo $directory
+    file_dir=$(dirname "$directory")
+    test_name=$(basename "$file_dir")
+    echo $file_dir
+done
+
