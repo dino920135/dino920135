5f025e83e6b8d08193d36d5a672974faa5999b58
Tsai Syun <65594575+dino920135@users.noreply.github.com>
2022-08-28 05:37:44 +0800

    Update GetListInRepo.sh

--- .github/workflows/GetListInRepo.sh
+++ .github/workflows/GetListInRepo.sh
@@ -4,15 +4,18 @@ git clone https://github.com/dino920135/Notes
 cd Notes
 
 echo "$PWD"
+echo
 
 data_path='./pages/'
 file_pattern='*.md'
 
-for directory in $(find ${data_path} -name ${file_pattern})
-do
-    # echo $directory
-    file_dir=$(dirname "$directory")
-    file_name=$(basename "$file_dir")
-    echo founnd $file_name
-done
+for dir in **/pages/*/; do basename "$dir"; done
+
+# for directory in $(find ${data_path} -name ${file_pattern})
+# do
+#     # echo $directory
+#     file_dir=$(dirname "$directory")
+#     file_name=$(basename "$file_dir")
+#     echo found $file_name
+# done
 
