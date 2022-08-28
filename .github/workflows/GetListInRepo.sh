aaa65df1fb33983c6913435baec38d9e9a414653
dino92135 <dino920135@gmail.com>
2022-08-28 08:20:59 +0800

    test

--- .github/workflows/GetListInRepo.sh
+++ .github/workflows/GetListInRepo.sh
@@ -17,17 +17,16 @@ echo "$PWD"
 sed '/BLOG-POST-LIST:START/,/BLOG-POST-LIST:END/d' ../../dino920135/README.md 
 cat ../../dino920135/README.md
 
-
-echo "<!-- BLOG-POST-LIST:START -->" >> ../../dino920135/README.md
-for dir in $PWD/*md
-do 
-  file_name=$(basename "$dir")
-  # Fixme replace spaces with %20
-  file_name_wospace=$file_name | sed -e "s/ /%20/g"
-  echo $file_name_wospace
-  echo "* [$file_name](https://github.com/dino920135/Notes/blob/main/pages/$file_name)" >> ../../dino920135/README.md
-done
-echo "<!-- BLOG-POST-LIST:END -->" >> ../../dino920135/README.md
+# echo "<!-- BLOG-POST-LIST:START -->" >> ../../dino920135/README.md
+# for dir in $PWD/*md
+# do 
+#   file_name=$(basename "$dir")
+#   # Fixme replace spaces with %20
+#   file_name_wospace=$file_name | sed -e "s/ /%20/g"
+#   echo $file_name_wospace
+#   echo "* [$file_name](https://github.com/dino920135/Notes/blob/main/pages/$file_name)" >> ../../dino920135/README.md
+# done
+# echo "<!-- BLOG-POST-LIST:END -->" >> ../../dino920135/README.md
 cat ../../dino920135/README.md
 
 # for directory in $(find ${data_path} -name ${file_pattern})
