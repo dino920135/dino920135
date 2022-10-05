4375fa727f762f722c4c099bcbeaeb5c3485b69b
Tsai Syun <65594575+dino920135@users.noreply.github.com>
2022-10-05 19:34:11 +0800

    Update GetListInRepo.sh

--- .github/workflows/GetListInRepo.sh
+++ .github/workflows/GetListInRepo.sh
@@ -18,9 +18,11 @@ sed -i '/.md]/d' ../../dino920135/README.md
 # cat ../../dino920135/README.md
 
 # echo "<!-- BLOG-POST-LIST:START -->" >> ../../dino920135/README.md
+counter=0
 #for dir in $(ls -t $PWD/*md | tail -n +5)
 for dir in $(git ls-files -z -- $PWD/*md | xargs -0n1 -I_ git --no-pager log -1 --date=iso-local --format="%ad _" -- $PWD/*md | sort)
 do 
+  counter=$(( counter + 1 ))
   #file_name=$(basename "$dir")
   file_name=$dir
   # Fixme replace spaces with %20
@@ -29,6 +31,7 @@ do
 #   str='- [$file_name](https://github.com/dino920135/Notes/blob/main/pages/$file_name)'
   sed -i "/BLOG-POST-LIST:START/a - [$file_name](https://github.com/dino920135/Notes/blob/main/pages/$file_name)" ../../dino920135/README.md
 #   echo "* [$file_name](https://github.com/dino920135/Notes/blob/main/pages/$file_name)" >> ../../dino920135/README.md
+  [ "$counter" -gt 10 ] && break
 done
 # echo "<!-- BLOG-POST-LIST:END -->" >> ../../dino920135/README.md
 cat ../../dino920135/README.md
