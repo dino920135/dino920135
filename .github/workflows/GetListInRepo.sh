0c81b0b88587f3e5df9a9ca59fab6be0166189cd
Tsai Syun <65594575+dino920135@users.noreply.github.com>
2025-04-15 15:43:01 +0800

    Update GetListInRepo.sh

--- .github/workflows/GetListInRepo.sh
+++ .github/workflows/GetListInRepo.sh
@@ -21,7 +21,7 @@ sed -i '/.md]/d' ../../dino920135/README.md
 # echo "<!-- BLOG-POST-LIST:START -->" >> ../../dino920135/README.md
 #for dir in $(ls -t $PWD/*md | tail -n +5)
 # for dir in $(git log --pretty='' --name-only -- $PWD/*.md| awk '!seen[$0]++' | head -n 10)
-for dir in $(git ls-tree --name-only -r HEAD -- *.md | sort -r | head -n 10)
+for dir in $(git log --name-only --pretty=format: -- '*.md' | sort -u | head -n 10)
 do 
   file_name=$(basename "$dir")
   page_name=${file_name%.md}
